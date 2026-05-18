b52daaa867ed4b2610bf97c1dba2bac02094d70f
Markku Kuismin <kuismima@univ.yo.oulu.fi>
2026-05-18 09:33:11 +0300

    Some editions and a figure demo.

--- functions/ar_dist.R
+++ functions/ar_dist.R
@@ -1,4 +1,4 @@
-ar_dist = function(x, h = "silverman"){
+ar_dist = function(x, h = "silverman", sim_dist = FALSE, M = 10^3){
   
   f0 = mvtnorm::dmvnorm(x)
   
@@ -10,10 +10,28 @@ ar_dist = function(x, h = "silverman"){
   
   rhos[is.na(rhos)] = 0
   
+  rhox = rhos
+  
   rhos[rhos > 1] = 1
   
   rho = mean(rhos)
   
-  return(list(rho = rho, rhos = rhos))
+  rho_sim = NULL
+  
+  if(sim_dist){
+    
+    rho_sim = rep(0, M)
+    
+    for(i in 1:M){
+      
+      u = runif(n)
+      
+      rho_sim[i] = mean(rhox > u)
+      
+    }
+    
+  }
+  
+  return(list(rho = rho, rhos = rhos, rho_sim = rho_sim))
   
 }
